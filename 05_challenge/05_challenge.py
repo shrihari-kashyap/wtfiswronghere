a872e14c2b167894e6b1f713f0cff5d6a8b35b60
Shrihari R <shrihari.r@qxf2.com>
2019-07-05 17:03:06 +0530

    05 challenge completed

--- 05_challenge/05_challenge.py
+++ 05_challenge/05_challenge.py
@@ -17,8 +17,8 @@ def fizzbuzz(max_num):
     # we will make our script 'tighter' in one of coming exercises
     three_mul = 'fizz'
     five_mul = 'buzz'
-    with open('mifile.txt','r') as f:
-        print 'i have created'
+    with open('myfile.txt','r') as f:
+        print ('i have created')
         num1 = int(f.readline())   
         num2=int(f.readline())        
         max_num = int(f.readline())
@@ -35,4 +35,4 @@ def fizzbuzz(max_num):
 
 #----START OF SCRIPT
 if __name__=='__main__':
-    fizzbuzz(100)
+    fizzbuzz(20)
